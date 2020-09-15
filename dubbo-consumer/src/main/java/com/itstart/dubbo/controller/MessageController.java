ee9ac75ccb4a81f5d473b692bae7c52794f612bf
ytlxy <a739771409@gmail.com>
2020-09-15 10:52:39 +0800

    new data

--- dubbo-consumer/src/main/java/com/itstart/dubbo/controller/MessageController.java
+++ dubbo-consumer/src/main/java/com/itstart/dubbo/controller/MessageController.java
@@ -4,13 +4,15 @@ import com.itstart.dubbo.service.IMessagesService;
 import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MessageController {
     @Reference
     private IMessagesService iMessagesService;
 
-    @RequestMapping
+    @RequestMapping("/helloMessage")
+    @ResponseBody
     public String helloMessage(String message){
         return iMessagesService.helloMessage(message);
     }
